704bb2f35ab027466a48b4162ac75bf3d0a764c9
Man <nguyenphanminhman04@gmail.com>
2026-02-04 08:47:56 +0700

    Just doing labs, includes reactjs: EX01, 02, 03

---
+++ Week01/src/App.jsx
@@ -0,0 +1,23 @@
+import ProductCard from "./component/productCard.jsx";
+import Button from "./component/Button.jsx";
+import Alert from "./component/Alert.jsx"
+function App() {
+  return (
+    /*
+    <div style={{ padding: "40px" }}>
+      <ProductCard />
+    </div>
+    */
+
+    /*<div style={{ padding: "40px" }}>
+      <Button type={"primary"}>Add to cart</Button>
+      <br />
+      <Button type={"danger"}>Delete</Button>
+      <br />
+      <Button type={"success"}>Confirm</Button>
+    </div>
+    */
+  );
+}
+
+export default App;
