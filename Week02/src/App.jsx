0690ded89d9a0c3897eeea579449f2ed092c0052
Man <nguyenphanminhman04@gmail.com>
2026-02-21 15:55:08 +0700

    Done Ex3 of the 2th week

--- Week02/src/App.jsx
+++ Week02/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import StudentInfo from "./components/StudentInfo";
 import CounterApp from "./components/CounterApp";
+import FormInput from "./components/FormInput";
 function App() {
   const sinhvien = {
     ten: "Nguyen Phan Minh Man",
@@ -9,7 +10,8 @@ function App() {
     lop: "DHKTPM18B",
   };
   return (
-  <CounterApp />
+    // <CounterApp />
+    <FormInput />
     /*<div>
       <Header />
       <StudentInfo ten={sinhvien.ten} mssv={sinhvien.mssv} lop={sinhvien.lop} />
--- Week02/src/components/FormInput.jsx
+++ Week02/src/components/FormInput.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react`";
-
+import { useState } from "react";
+import "../../styles/FormInput.css";
 export default function FormInput() {
   const [form, setForm] = useState({
     name: "",
@@ -13,28 +13,35 @@ export default function FormInput() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h3>Information</h3>
-      Your name:{" "}
-      <input
-        name="name"
-        type="text"
-        onChange={handleChange}
-        value={form.name}
-        placeholder="Your name"
-      />
-      Your email:{" "}
-      <input
-        name="email"
-        type="text"
-        onChange={handleChange}
-        value={form.email}
-        placeholder="Your email"
-      />
+      <div className="form-container">
+        <strong>Your name:</strong>
+
+        <input
+          name="name"
+          type="text"
+          onChange={handleChange}
+          value={form.name}
+          placeholder="Your name"
+        />
+
+        <strong>Your email:</strong>
+        <input
+          name="email"
+          type="text"
+          onChange={handleChange}
+          value={form.email}
+          placeholder="Your email"
+        />
+      </div>
+
       <hr />
       <h3>Preview</h3>
-      <p>Name: {form.name}</p>
-      <p>Email: {form.email}</p>
+      <div className="preview">
+        <p>Name: {form.name}</p>
+        <p>Email: {form.email}</p>
+      </div>
     </div>
   );
 }
