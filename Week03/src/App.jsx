fc54483aac5f04b32669110bd3ac180115d86a48
Man <nguyenphanminhman04@gmail.com>
2026-03-21 16:27:44 +0700

    feat(week03): i'm done ex04 React.memo, useCallback

--- Week03/src/App.jsx
+++ Week03/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Bai01 from "./components/Btap01";
 import Bai02 from "./components/Baitap02";
 import Bai03 from "./components/Btap03";
+import Bai04 from "./components/Btap04";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* <Bai01/> */}
       {/* <Bai02 /> */}
       {/* <Bai03 /> */}
+      <Bai04 />
     </>
   );
 }
