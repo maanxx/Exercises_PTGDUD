8077158d37bdc0793878a1f21a16ecb854543ecb
Man <nguyenphanminhman04@gmail.com>
2026-03-25 02:07:27 +0700

    feat(week04): bai05

--- Week04/src/App.jsx
+++ Week04/src/App.jsx
@@ -3,6 +3,7 @@ import Bai01 from "./components/Bai01";
 import Bai02 from "./components/Bai02";
 import Bai03 from "./components/Bai03";
 import Bai04 from "./components/Bai04";
+import Bai05 from "./components/Bai05";
 
 function App() {
   return (
@@ -13,7 +14,10 @@ function App() {
 
       {/* <Bai03 /> */}
 
-      <Bai04/>
+      {/* <Bai04/> */}
+
+      <Bai05/>
+      
     </>
   );
 }
