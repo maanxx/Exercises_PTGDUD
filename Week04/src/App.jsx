517f84b6828a57825d20d0996317ed335f5672a2
Man <nguyenphanminhman04@gmail.com>
2026-03-25 00:18:57 +0700

    feat(week04): fetch id api Dynamic Fetch

--- Week04/src/App.jsx
+++ Week04/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Bai01 from "./components/Bai01";
 import Bai02 from "./components/Bai02";
+import Bai03 from "./components/Bai03";
 
 function App() {
   return (
     <>
       {/* <Bai01 /> */}
 
-      <Bai02/>
+      {/* <Bai02/> */}
+
+      <Bai03 />
     </>
   );
 }
