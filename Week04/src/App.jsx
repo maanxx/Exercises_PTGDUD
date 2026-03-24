1f9d23fbdf15ff5bed3ac088ac6812ad9704cac4
Man <nguyenphanminhman04@gmail.com>
2026-03-25 01:39:15 +0700

    feat(week04): filter input

--- Week04/src/App.jsx
+++ Week04/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Bai01 from "./components/Bai01";
 import Bai02 from "./components/Bai02";
 import Bai03 from "./components/Bai03";
+import Bai04 from "./components/Bai04";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
 
       {/* <Bai02/> */}
 
-      <Bai03 />
+      {/* <Bai03 /> */}
+
+      <Bai04/>
     </>
   );
 }
